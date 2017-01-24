d67019ca6b44166f06f53c5c458ec16684879b89
shokohara <shokohara@users.noreply.github.com>
2017-01-24 13:33:37 +0900

    wip

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,7 +1,18 @@
 package sandbox
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("sandbox")
+class B {
+  def v: Int = {
+    println("evaluated")
+    1
   }
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val f = x.apply()
+    println(f.v) // ここでB#vを評価したい
+  }
+
+  def x[A >: {def v : Int}]: () => A = new B().v _
 }
