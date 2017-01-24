58df935a5a62e653aad0ffbb7e4f2b520f71322e
shokohara <shokohara@users.noreply.github.com>
2017-01-24 18:00:14 +0900

    mem

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,38 +1,29 @@
 package sandbox
 
-trait Problem[A] {
-  def resolve(f: A): Int
+class A()
+object A {
+  def x = "testdata"
 }
 
-object Problem {
-  def apply[A: Problem] = implicitly[Problem[A]]
-}
-
-class Problem01()
-
-object Problem01 {
-  implicit val problem01Problem = new Problem[Problem01] {
-    def resolve(b: Problem01) = "take many cpu time to find a answer".length
-  }
-}
-
-class Problem02()
-
-object Problem02 {
-  implicit val problem02Problem = new Problem[Problem02] {
-    def resolve(b: Problem02) = "really take too many cpu time to find a answer".length
-  }
+class B() {
+  def x = "testdata"
 }
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Problem[Problem01].resolve(new Problem01)
-    // success
-    //    val solutions = ((1, Problem[Problem01]) :: Nil).toMap
-    //    solutions.get(1).get.resolve(new Problem01)
-    // failed
-    //    val solutions = ((1, Problem01) :: (2, Problem02) :: Nil).toMap
-    //    solutions.get(1).get.resolve(new Problem01)
+    val  n = 10000000
+//    val as = (1 to n).map(_ => new A); println("a");
+    val as = (1 to n).map(_ => new B); println("b");
+    System.gc()
+    val total = Runtime.getRuntime().totalMemory();
+    val free = Runtime.getRuntime().freeMemory();
+    val used = total - free;
+    val max = Runtime.getRuntime().maxMemory();
+    System.out.println("total => " + total / 1024 + "KB");
+    System.out.println("free  => " + free / 1024 + "KB");
+    System.out.println("used  => " + (total - free) / 1024 + "KB");
+    System.out.println("max   => " + max / 1024 + "KB");
+    as.length
   }
 }
